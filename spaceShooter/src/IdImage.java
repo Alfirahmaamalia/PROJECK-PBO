eaa1b3f2a005f05e4803556a35528ade61fbe0dc
Alfirahmaamalia <144764310+Alfirahmaamalia@users.noreply.github.com>
2024-12-14 17:03:52 +0700

    Update IdImage.java

---
+++ spaceShooter/src/IdImage.java
@@ -0,0 +1,20 @@
+import javafx.scene.image.Image;
+
+//menjadikan gambar sebagai variabel
+public class IdImage {
+    public final Image blueShip = new Image("playerShip/pesawat1.png");
+    public final Image redShip = new Image("playerShip/pesawat2.png");
+    public final Image greenShip = new Image("playerShip/pesawat3.png");
+    public final Image singleLaser = new Image("playerLaser/playerLaser.png");
+    public final Image doubleLaser = new Image("playerLaser/playerLaser.png");
+    public final Image rocket = new Image("playerLaser/bomb.png");
+    public final Image enemyBlueLvl1 = new Image("enemyShip/enemyBlueLvl1.png");
+    public final Image enemyOrangeLvl1 = new Image("enemyShip/enemyOrangeLvl1.png");
+    public final Image enemyBlackLvl2 = new Image("enemyShip/enemyBlackLvl2.png");
+    public final Image enemyGreenLvl2 = new Image("enemyShip/enemyGreenLvl2.png");
+    public final Image doubleLaserIv = new Image("playerLaser/LaserBG.png");
+    public final Image enemyLaserLvl1 = new Image("enemyLaser/enemyLaserLvl1.png");
+    public final Image enemyLaserLvl2 = new Image("enemyLaser/enemyLaserLvl2.png");
+    public final Image background = new Image("background/spaceBG.png");
+    public final Image explosionEnemy = new Image("background/explosionEnemy.png");
+}
