1c6c8cf95716c7a77d054b1235cf7d90d2c4a187
MN Faadil <121507446+bugsm@users.noreply.github.com>
2024-12-14 14:28:08 +0700

    Update EndGameWindow.java

---
+++ spaceShooter/src/EndGameWindow.java
@@ -0,0 +1,32 @@
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class EndGameWindow implements Initializable {
+
+    @FXML
+    private Label scoreLabel;
+
+    //untuk memulai game kembali
+    @FXML
+    private void playAin() throws IOException {
+        Parent parent = FXMLLoader.load(getClass().getResource("FXML/mainScreen.fxml"));
+        Stage stage = (Stage) scoreLabel.getScene().getWindow();
+        stage.setScene(new Scene(parent));
+
+    }
+    //Menampilkan Score
+    @override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        PlayerController game = new PlayerController();
+        scoreLabel.setText("Your score: " + game.getScore());
+    }
+}
